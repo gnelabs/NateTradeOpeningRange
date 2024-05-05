4934d87a1079e1ef8329306bcd1e9c4b0306514f
gnelabs <gnelabs1@gmail.com>
2024-05-04 22:51:45 -0400

    Refactored engine to remove logic bugs, make debugging easier.

--- backtest/engine.py
+++ backtest/engine.py
@@ -2,6 +2,8 @@
 __author__ = "Nathan Ward"
 
 import logging
+from sys import stdout
+from collections import defaultdict
 from statistics import fmean
 
 _LOGGER = logging.getLogger()
@@ -10,28 +12,69 @@ _LOGGER.setLevel(logging.INFO)
 
 class ProcessOpeningRanges(object):
     def __init__(self):
-        #Adjustable stop distance for trading. To-do: Research this
+        #Adjustable stop distance for trading.
         self.stop_distance = 0.25
 
-        #Adjustable count of times stop loss is hit. To-do: Research this
+        #Adjustable count of times stop loss is hit.
         self.stop_count_limit = 4
 
         #Adjustable cooloff period in seconds between the stop getting hit before the next trade can commence.
         self.stop_cooloff_period = 30
 
-        #Adjustable take profit distance. To-do: Research this
+        #Adjustable take profit distance.
         self.limit_distance = 5
     
-    def backtest(self, open_price:float, range_high:float, range_low:float, intraday_data:list) -> dict:
+    def compress_time_series(self, agg_data_raw:dict) -> dict:
         """
-        Using intraday data collected for each day, perform analysis based on breakouts from the
-        opening range as provided.
-        """
-        #Keep track of how long the holding period is in seconds for each trade to do risk analysis.
-        holding_period = []
+        Take raw intra-second time series data from the database and compress it by 
+        removing duplicates and sort it by price. Although the raw data is more 
+        visually appearing because it can be charted, it is not optimized for minimal 
+        processing iterations. This takes data that looks like:
 
-        #Keep track of the profit for each trade.
-        trade_tracker = []
+        {'2023-04-24': [
+                {'timestamp_utc': 1682343030, 'underlying': 411.99},
+                {'timestamp_utc': 1682343030, 'underlying': 411.99},
+                {'timestamp_utc': 1682343031, 'underlying': 411.99},
+                {'timestamp_utc': 1682343032, 'underlying': 411.98},
+            ]
+        }
+
+        To this:
+
+        {'2023-04-24': {
+                1682343030: 411.99,
+                1682343032: 411.98
+            }
+        }
+        """
+        last_timestamp = 0
+        last_price_entered = 0
+        compressed_data = defaultdict(dict)
+
+        for k_date, v_intraday_data_raw in agg_data_raw.items():
+            for item in v_intraday_data_raw:
+                if not last_timestamp:
+                    last_timestamp = item['timestamp_utc']
+                    last_price_entered = item['underlying']
+                    compressed_data[k_date][item['timestamp_utc']] = item['underlying']
+                else:
+                    #These are ints, so increment by at least one second.
+                    if item['timestamp_utc'] >= last_timestamp:
+                        if item['underlying'] != last_price_entered:
+                            compressed_data[k_date][item['timestamp_utc']] = item['underlying']
+                            last_price_entered = item['underlying']
+                            last_timestamp = item['timestamp_utc']
+        
+        return compressed_data
+
+    def backtest_redux(self, opening_range_info:dict, compressed_agg_data:dict) -> dict:
+        """
+        Using opening range information and intraday price data, perform a backtest.
+
+        Redux: Re-wrote this logic to make it clearer.
+        """
+        #Per-trade information. Things like holding period, p&l, cost basis.
+        trade_stats = defaultdict(dict)
 
         #Holding object for stop price.
         stop_price = 0
@@ -39,82 +82,119 @@ class ProcessOpeningRanges(object):
         #Holding object for limit price.
         limit_price = 0
 
-        #To simplify logic, indicate is trade is long or short.
-        trade_is_long = False
-
         #Count of how many times the stop was hit.
         stop_triggered_count = 0
+
+        #Count of times trade has initiated.
+        trade_initiated_count = 0
 
         #Stop cooloff timestamp, used to time the cooloff period.
         stop_cooloff_timestamp = 0
 
-        for data in intraday_data:
+        #Indicate if there is an active position on or not.
+        active_position_long = False
+        active_position_short = False
+
+        #Opening range information.
+        range_high = opening_range_info['high']
+        range_low = opening_range_info['low']
+        day_open_price = opening_range_info['open_price']
+
+        #End of the trading day.
+        end_of_trading_day_timestamp = list(compressed_agg_data.keys())[-1]
+
+        for k_timestamp, v_price in compressed_agg_data.items():
             #Check to see if the stop has reached the risk limit.
+            #Skip further processing for the day if that is the case.
             if stop_triggered_count == self.stop_count_limit:
                 break
-            
+
             #Check to see if the stop was hit last iteration and needs to cool off.
-            if data['timestamp_utc'] < stop_cooloff_timestamp:
+            #If the cooldown period is active, skip processing this timestamp.
+            if k_timestamp < stop_cooloff_timestamp:
                 continue
 
-            #stop_price being falsy indicates no active trade, so trigger one.
-            if not stop_price:
+            #No position, check ranges.
+            if not active_position_long and not active_position_short:
                 #Bullish breakout above the opening range.
-                if data['underlying'] > range_high:
-                    trade_tracker.append(data['underlying'])
-                    holding_period.append(data['timestamp_utc'])
-                    trade_is_long = True
-                    stop_price = data['underlying'] - self.stop_distance
-                    limit_price = data['underlying'] + self.limit_distance
-                #Bearish breakout below the opening range.
-                elif data['underlying'] < range_low:
-                    trade_tracker.append(data['underlying'])
-                    holding_period.append(data['timestamp_utc'])
-                    trade_is_long = False
-                    stop_price = data['underlying'] + self.stop_distance
-                    limit_price = data['underlying'] - self.limit_distance
+                if v_price > range_high:
+                    active_position_long = True
+                    stop_price = v_price - self.stop_distance
+                    limit_price = v_price + self.limit_distance
+                    trade_initiated_count += 1
+                    trade_stats[trade_initiated_count] = {
+                        'trade_open_price': v_price,
+                        'timestamp_opened': k_timestamp,
+                        'direction': 'long'
+                    }
+                #Bearish breakdown below the opening range.
+                elif v_price < range_low:
+                    active_position_short = True
+                    stop_price = v_price + self.stop_distance
+                    limit_price = v_price - self.limit_distance
+                    trade_initiated_count += 1
+                    trade_stats[trade_initiated_count] = {
+                        'trade_open_price': v_price,
+                        'timestamp_opened': k_timestamp,
+                        'direction': 'short'
+                    }
+            #There is an active position.
             else:
-                #Bullish breakout logic
-                if trade_is_long:
-                    #Profit taken
-                    if data['underlying'] > limit_price or data == intraday_data[-1]:
-                        trade_tracker[-1] = limit_price - trade_tracker[-1]
-                        holding_period[-1] = data['timestamp_utc'] - holding_period[-1]
-                        stop_price = 0
-                        limit_price = 0
+                if active_position_long:
+                    if v_price >= limit_price or k_timestamp == end_of_trading_day_timestamp:
+                        #Reached the limit or end of day, take profit and close the position.
+                        trade_stats[trade_initiated_count].update({
+                            'trade_close_price': v_price,
+                            'profit': v_price - trade_stats[trade_initiated_count]['trade_open_price'],
+                            'holding_period': k_timestamp - trade_stats[trade_initiated_count]['timestamp_opened'],
+                            'timestamp_closed': k_timestamp
+                        })
+                        #Since this is a trend following strategy, once profit has been achieved, no 
+                        #further trading for the day.
                         break
-                    #Stop hit on the downside
-                    elif data['underlying'] < stop_price or data == intraday_data[-1]:
-                        trade_tracker[-1] = stop_price - trade_tracker[-1]
-                        holding_period[-1] = data['timestamp_utc'] - holding_period[-1]
-                        stop_price = 0
-                        limit_price = 0
+                    elif v_price <= stop_price:
+                        #Stopped out, take the loss and start a cooldown period.
+                        stop_cooloff_timestamp = k_timestamp + self.stop_cooloff_period
                         stop_triggered_count += 1
-                        stop_cooloff_timestamp = data['timestamp_utc'] + self.stop_cooloff_period
-                #Bearish breakout logic
-                else:
-                    #Profit taken
-                    if data['underlying'] < limit_price or data == intraday_data[-1]:
-                        trade_tracker[-1] = trade_tracker[-1] - limit_price
-                        holding_period[-1] = data['timestamp_utc'] - holding_period[-1]
+                        trade_stats[trade_initiated_count].update({
+                            'trade_close_price': v_price,
+                            'profit': v_price - trade_stats[trade_initiated_count]['trade_open_price'],
+                            'holding_period': k_timestamp - trade_stats[trade_initiated_count]['timestamp_opened'],
+                            'timestamp_closed': k_timestamp
+                        })
                         stop_price = 0
                         limit_price = 0
+                        active_position_long = False
+                elif active_position_short:
+                    if v_price <= limit_price or k_timestamp == end_of_trading_day_timestamp:
+                        #Reached the limit or end of the day, take profit and close the position.
+                        trade_stats[trade_initiated_count].update({
+                            'trade_close_price': v_price,
+                            'profit':  v_price - trade_stats[trade_initiated_count]['trade_open_price'],
+                            'holding_period': k_timestamp - trade_stats[trade_initiated_count]['timestamp_opened'],
+                            'timestamp_closed': k_timestamp
+                        })
+                        #Since this is a trend following strategy, once profit has been achieved, no 
+                        #further trading for the day.
                         break
-                    #Stop hit on the upside
-                    elif data['underlying'] > stop_price or data == intraday_data[-1]:
-                        trade_tracker[-1] = trade_tracker[-1] - stop_price
-                        holding_period[-1] = data['timestamp_utc'] - holding_period[-1]
+                    elif v_price >= stop_price:
+                        #Stopped out, take the loss and start a cooldown period.
+                        stop_cooloff_timestamp = k_timestamp + self.stop_cooloff_period
+                        stop_triggered_count += 1
+                        trade_stats[trade_initiated_count].update({
+                            'trade_close_price': v_price,
+                            'profit': trade_stats[trade_initiated_count]['trade_open_price'] - v_price,
+                            'holding_period': k_timestamp - trade_stats[trade_initiated_count]['timestamp_opened'],
+                            'timestamp_closed': k_timestamp
+                        })
                         stop_price = 0
                         limit_price = 0
-                        stop_triggered_count += 1
-                        stop_cooloff_timestamp = data['timestamp_utc'] + self.stop_cooloff_period
-                    
-        
+                        active_position_short = False
+
         return {
-            'count_stops_triggered': stop_triggered_count,
-            'count_trades_triggered': len(trade_tracker),
-            'holding_period_data': holding_period,
-            'average_holding_period_per_trade': fmean(holding_period),
-            'trade_tracker': trade_tracker,
-            'net_profit': sum(trade_tracker)
+            'stops_triggered': stop_triggered_count,
+            'trades_triggered': trade_initiated_count,
+            'average_holding_period': fmean(k['holding_period'] for k in trade_stats.values()),
+            'net_profit': sum(k['profit'] for k in trade_stats.values()),
+            'trade_stats': trade_stats
         }
